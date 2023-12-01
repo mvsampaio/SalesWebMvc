3c4335aa5a46996735eb2b47a06361144c196a4e
Marcus Sampaio <sampaiosoftware@gmail.com>
2023-12-01 16:36:26 -0300

    Simple create form and add foreign key DepartmentId

---
+++ SalesWebMvc/Migrations/20231201191510_DepartmentForeignKey.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SalesWebMvc.Migrations
+{
+    public partial class DepartmentForeignKey : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_SalesRecord_Seller_SellerId",
+                table: "SalesRecord");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Seller_Department_DepartmentId",
+                table: "Seller");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "DepartmentId",
+                table: "Seller",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "SellerId",
+                table: "SalesRecord",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SalesRecord_Seller_SellerId",
+                table: "SalesRecord",
+                column: "SellerId",
+                principalTable: "Seller",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Seller_Department_DepartmentId",
+                table: "Seller",
+                column: "DepartmentId",
+                principalTable: "Department",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_SalesRecord_Seller_SellerId",
+                table: "SalesRecord");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Seller_Department_DepartmentId",
+                table: "Seller");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "DepartmentId",
+                table: "Seller",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "SellerId",
+                table: "SalesRecord",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SalesRecord_Seller_SellerId",
+                table: "SalesRecord",
+                column: "SellerId",
+                principalTable: "Seller",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Seller_Department_DepartmentId",
+                table: "Seller",
+                column: "DepartmentId",
+                principalTable: "Department",
+                principalColumn: "Id");
+        }
+    }
+}
